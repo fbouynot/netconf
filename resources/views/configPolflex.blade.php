1f2d70d940572f7bf605fe8fd51e66894d5e8d66
fbouynot <felixbouynot@gmail.com>
2022-10-06 13:43:44 +0200

    First factorization

--- resources/views/configPolflex.blade.php
+++ resources/views/configPolflex.blade.php
@@ -6,8 +6,15 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Polflex creation form') }}</div>
-                    <p>OK</p>
-                </div>
+                    @include('fortigateRule', array(
+                                                'srcintf' => 'internal',
+                                                'dstintf' => 'wan1',
+                                                'srcaddr' => 'all',
+                                                'dstaddr' => 'all',
+                                                'action' => 'accept',
+                                                'services' => '"HTTP" "HTTPS"',
+                                                'nat' => 'enable'
+                                                ))
             </div>
         </div>
     </div>
